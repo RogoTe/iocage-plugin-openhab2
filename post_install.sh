43dee68ace892ac435328845dd2cdbda4b9efafc
RogoTe <45861130+RogoTe@users.noreply.github.com>
2019-09-24 15:05:27 +0200

    Add link to installation manual

--- post_install.sh
+++ post_install.sh
@@ -7,3 +7,4 @@ sysrc openhab2_java_opts="-Djava.net.preferIPv4Stack=true"
 # Start the service
 service openhab2 onestart  2>/dev/null
 
+echo "Please follow the Installation Instructions at https://www.openhab.org/docs/tutorial/1sttimesetup.html" > /root/PLUGIN_INFO
